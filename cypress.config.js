263dcd7c4a7c6e232c70c9bb2f4daa952ffa2d87
Gui Cerutti <guilherme.cerutti@compliancestation.com.br>
2024-12-25 21:51:24 -0300

    create e2e tests with cypress

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  chromeWebSecurity: false,
+  e2e: {
+    defaultCommandTimeout: 40000,
+    baseUrl: 'https://notes-serverless-app.com',
+    env: {
+      viewportWidthBreakpoint: 768,
+    },
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+
+    },
+  },
+})
