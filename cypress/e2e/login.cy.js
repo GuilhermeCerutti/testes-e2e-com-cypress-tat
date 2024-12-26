0d9c1f84cdcebaec683444831eeb5996907ab66d
Gui Cerutti <guilherme.cerutti@compliancestation.com.br>
2024-12-25 22:01:55 -0300

    fix

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,7 @@
 describe('Login', () => {
   it('successfully logs in', () => {
     cy.intercept('GET', '**/notes').as('getNotes')
-
-   cy.guiLogin()
-
-
+    cy.guiLogin()
     cy.contains('a', 'Create a new note').should('be.visible')
   })
 })
